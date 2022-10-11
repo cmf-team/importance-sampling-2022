4b0ccd9cba4655f4a76fa1e2731dae79ab013947
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2022-10-11 11:48:59 +0600

    Update data.py

--- data.py
+++ data.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 
 def stocks_returns(assets, weights, from_date, to_date):
-    raise Exception(NotImplementedError)
+    pass
 
 def commodities_returns(assets, weights, from_date, to_date):
-    raise Exception(NotImplementedError)
+    pass
 
 def cryptocurrencies_returns(assets, weights, from_date, to_date):
-    raise Exception(NotImplementedError)
+    pass
