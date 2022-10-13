65b90c9f6b788942053cca5f3e31c06990d485e4
Mariia Zykova <73541885+MiaZym@users.noreply.github.com>
2022-10-13 17:16:35 +0300

    update data.py
    +change requirements

--- data.py
+++ data.py
@@ -3,6 +3,7 @@
 """
 
 import investiny
+import pandas as pd
 import numpy as np
 from datetime import timedelta, datetime
 
@@ -25,34 +26,39 @@ def get_lst_assets(assets, from_date, to_date, types = "False", exchange = "Fals
                 results = investiny.search_assets(query=name, limit=1, type=types, exchange="NASDAQ")
             else:
                 results = investiny.search_assets(query=name, limit=1, type=types)
+
         investing_id = int(results[0]["ticker"])
         data = investiny.historical_data(investing_id=investing_id, from_date=from_date, to_date=to_date)
+        lst_date = data['date']
         lst.append(data['close'])
     
-    return np.array(lst)
+    return pd.DataFrame(data = np.array(lst), columns = lst_date)
 
 
 #создание портфеля
 def profit(data, weights):
-    result = []
-    new_data = np.array([data[i,:]*weights[i] for i in range(len(data))])
-    new_data = new_data.sum(axis=0) #перемножение данных с весами и сложение
+    #перемножение данных с весами и сложение
+    new_data = pd.DataFrame([data.loc[i]*weights[i] for i in range(len(data))]).sum(axis=0)
     
     #вычисление returns для портфеля
+    result = new_data.copy(deep=True)
     for i in range(1, len(new_data)):
-        result.append((new_data[i] - new_data[i-1])/new_data[i-1])
+        result[i] = (new_data[i] - new_data[i-1])/new_data[i-1]
 
-    return result
+    return result[1:]
 
 
 def stocks_returns(assets, weights, from_date, to_date):
+    from_date = change_date(from_date,-1)
+    to_date = change_date(to_date,1)
     data = get_lst_assets(assets, from_date, to_date, types="Stock", exchange="NASDAQ")
     returns = profit(data, weights)
     
     return returns
 
-
 def commodities_returns(assets, weights, from_date, to_date):
+    from_date = change_date(from_date,-1)
+    to_date = change_date(to_date,1)
     data = get_lst_assets(assets, from_date, to_date, types="Commodity")
     returns = profit(data, weights)
     
@@ -60,15 +66,12 @@ def commodities_returns(assets, weights, from_date, to_date):
 
 
 def cryptocurrencies_returns(assets, weights, from_date, to_date):
+    from_date = change_date(from_date,-1)
+    to_date = change_date(to_date,1)
     data = get_lst_assets(assets, from_date, to_date)
     returns = profit(data, weights)
     
     return returns
-
-
-
-
-
 
 
 
