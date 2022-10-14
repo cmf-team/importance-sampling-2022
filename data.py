a1d07467e72f29b1ee665ada5e40e816538d784c
kids_seek_options <99182431+Lycher2@users.noreply.github.com>
2022-10-14 09:55:45 +0300

    Update data.py

--- data.py
+++ data.py
@@ -3,12 +3,12 @@ import datetime as dt
 import numpy as np
 import investiny as inv
 
-def stocks_returns(assets, weights, from_date, to_date):
+def get_yield(assets, weights, from_date, to_date, **exchange):
     returns = pd.DataFrame()
     from_date = (dt.datetime.strptime(from_date, '%m/%d/%Y')-dt.timedelta(days=1)).strftime('%m/%d/%Y')
     to_date= (dt.datetime.strptime(to_date, '%m/%d/%Y')+dt.timedelta(days=1)).strftime('%m/%d/%Y')
     for asset in assets:
-        ticker = int(inv.search_assets(query=asset, limit=1, exchange = 'NASDAQ')[0]['ticker'])
+        ticker = int(inv.search_assets(query=asset, limit=1, **exchange)[0]['ticker'])
         data = pd.DataFrame(inv.historical_data(investing_id=ticker, from_date=from_date, to_date=to_date)).set_index('date').drop(columns=['open','low','volume','high'])
         returns = pd.concat([returns,data.diff().rename(columns={'close': 'diff_'+asset}),data.rename(columns={'close': asset})],axis = 1)
     del data
@@ -19,3 +19,11 @@ def stocks_returns(assets, weights, from_date, to_date):
         returns['widiff'] += returns['diff_'+assets[i]] * weights[i]
     returns['wipim1'] = returns.wipim1.shift(1)
     return (returns['widiff']/returns['wipim1']).dropna();
+def stocks_returns(assets, weights, from_date, to_date):
+    return get_price(assets, weights, from_date, to_date, exchange = 'NASDAQ')
+    
+def commodities_returns(assets, weights, from_date, to_date):
+    return get_price(assets, weights, from_date, to_date, exchange = 'ICE')
+
+def cryptocurrencies_returns(assets, weights, from_date, to_date):
+    return get_price(assets, weights, from_date, to_date, exchange = 'Binance')
