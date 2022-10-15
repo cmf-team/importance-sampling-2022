a6aa34b126db15a14cadaddcfe1e995d8885a6f8
barracudada <skirill720@yandex.ru>
2022-10-16 00:21:14 +0300

    final

--- data.py
+++ data.py
@@ -1,11 +1,45 @@
 import pandas as pd
 import numpy as np
+import investiny
 
 def stocks_returns(assets, weights, from_date, to_date):
-    raise Exception(NotImplementedError)
+    st_data = []
+    i = 0
+    for sec in assets:
+        sec_id = investiny.search_assets(query=sec, limit=1, exchange="NASDAQ")[0]['ticker']
+        data = investiny.historical_data(sec_id, from_date, to_date)
+        dates = data['date']
+        st_data.append([j * weights[i] for j in data['close']])
+        i += 1
+    res = pd.DataFrame(data = np.array(st_data), columns = dates).T 
+    res = res.sum(axis=1)
+    res = (res.diff()/res.shift(1)).dropna()
+    return(res)
 
 def commodities_returns(assets, weights, from_date, to_date):
-    raise Exception(NotImplementedError)
+    st_data = []
+    i = 0
+    for sec in assets:
+        sec_id = investiny.search_assets(query=comm[0], limit=1, type='Commodity')[0]["ticker"]
+        data = investiny.historical_data(sec_id, from_date, to_date)
+        dates = data['date']
+        st_data.append([j * weights[i] for j in data['close']])
+        i += 1
+    res = pd.DataFrame(data = np.array(st_data), columns = dates).T 
+    res = res.sum(axis=1)
+    res = (res.diff()/res.shift(1)).dropna()
+    return(res)
 
 def cryptocurrencies_returns(assets, weights, from_date, to_date):
-    raise Exception(NotImplementedError)
+    st_data = []
+    i = 0
+    for sec in assets:
+        sec_id = investiny.search_assets(query=comm[0], limit=1)[0]["ticker"]
+        data = investiny.historical_data(sec_id, from_date, to_date)
+        dates = data['date']
+        st_data.append([j * weights[i] for j in data['close']])
+        i += 1
+    res = pd.DataFrame(data = np.array(st_data), columns = dates).T 
+    res = res.sum(axis=1)
+    res = (res.diff()/res.shift(1)).dropna()
+    return(res)
--- test.py
+++ test.py
@@ -1,22 +1,63 @@
-import numpy as np
 import pandas as pd
-from data import stocks_returns
+import numpy as np
+import investiny
+from investiny import historical_data, search_assets
+from datetime import timedelta, date, datetime
 
-def test_stocks_returns():
-    assets = ['AAPL']
-    weights = [1.]
-    returns = stocks_returns(assets, weights, from_date='09/02/2022', to_date='09/07/2022')
-    test_returns = pd.Series(
-        data=[-0.0136, -0.0082, 0.0093], 
-        index=pd.to_datetime(['09/02/2022', '09/06/2022', '09/07/2022']),
-    )
-    assert np.allclose(returns, test_returns, atol=0.0001)
+
+def change_date(date, days_count):
+    to_date_tmp = datetime.strptime(date, '%m/%d/%Y')
+    to_date_tmp = to_date_tmp + timedelta(days=days_count)
+    date = to_date_tmp.strftime('%m/%d/%Y')
+    return(date)
+
+def get_lst_assets(assets, from_date, to_date, types = "False", exchange = "False"):
+    lst= []
+    for name in assets:
+        if types == "False":
+            results = investiny.search_assets(query=name, limit=1)
+        else:
+            if exchange != "False":
+                results = investiny.search_assets(query=name, limit=1, type=types, exchange="NASDAQ")
+            else:
+                results = investiny.search_assets(query=name, limit=1, type=types)
+
+        investing_id = int(results[0]["ticker"])
+        data = investiny.historical_data(investing_id=investing_id, from_date=from_date, to_date=to_date)
+        lst_date = data['date']
+        lst.append(data['close'])
+    res = pd.DataFrame(data = np.array(lst), columns = lst_date)
+    return(res)
+
+def stocks_returns(assets, weights, from_date, to_date):
+    from_date = change_date(from_date, -1)
+    to_date = change_date(to_date, 1)
     
-    assets = ['AAPL', 'GOOGL']
-    weights = [0.3, 0.7]
-    returns = stocks_returns(assets, weights, from_date='09/02/2022', to_date='09/07/2022')
-    test_returns = pd.Series(
-        data=[-0.0158, -0.0091, 0.0188], 
-        index=pd.to_datetime(['09/02/2022', '09/06/2022', '09/07/2022']),
-    )
-    assert np.allclose(returns, test_returns, atol=0.0001)
+    x = get_lst_assets(assets, from_date=from_date, to_date=to_date)
+    x = x.T
+    x.index = pd.to_datetime(x.index)
+    for col in list(x.columns):
+        x[col] = x[col] * weights[col]
+    x = x.sum(axis=1)
+    x = (x.diff()/x.shift(1)).dropna()
+    return(x)
+
+def commodities_returns(assets, weights, from_date, to_date):
+    x = get_lst_assets(assets, from_date=from_date, to_date=to_date)
+    x = x.T
+    x.index = pd.to_datetime(x.index)
+    for col in list(x.columns):
+        x[col] = x[col] * weights[col]
+    x = x.sum(axis=1)
+    x = (x.diff()/x.shift(1)).dropna()
+    return(x)
+
+def cryptocurrencies_returns(assets, weights, from_date, to_date):
+    x = get_lst_assets(assets, from_date=from_date, to_date=to_date)
+    x = x.T
+    x.index = pd.to_datetime(x.index)
+    for col in list(x.columns):
+        x[col] = x[col] * weights[col]
+    x = x.sum(axis=1)
+    x = (x.diff()/x.shift(1)).dropna()
+    return(x)
