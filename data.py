aa6d67712b85bd10dde8174d6afba5a03851dc79
13arthur11 <93545829+13arthur11@users.noreply.github.com>
2022-10-15 11:13:08 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import investiny
-import pandas as pd
 import numpy as np
+import pandas as pd
+# import numpy as np
 import yfinance as yf
 from investiny import historical_data, search_assets
 
@@ -10,14 +11,14 @@ def com_n_crypto(assets, weights, from_date, to_date):
     asset_number = 0
     arr_of_day_price = []
     to_date = pd.to_datetime(to_date) + pd.DateOffset(1)
-    to_date =  str(to_date.month) + "/" + str(to_date.day) + "/" + str(to_date.year)
-    print(to_date)
+    to_date = str(to_date.month) + "/" + str(to_date.day) + "/" + str(to_date.year)
+    from_date = pd.to_datetime(to_date) - pd.DateOffset(1)
+    from_date = str(to_date.month) + "/" + str(to_date.day) + "/" + str(to_date.year)
     for asset in assets:
         history = historical_data(investiny.search_assets(query=asset, limit=1)[0]["ticker"], from_date=from_date,
                                   to_date=to_date)
         date_history = history["date"]
         price_history = history["close"]
-
 
         for price in range(0, len(price_history)):
             if (len(arr_of_day_price) - 1 < price):
@@ -26,15 +27,16 @@ def com_n_crypto(assets, weights, from_date, to_date):
                 arr_of_day_price[price] += price_history[price] * weights[asset_number % len(assets)]
         asset_number += 1
 
-    for day in range(1, len(price_history)):
+    for day in range(0, len(price_history)):
         # if stock_data doesn't have an element at index pd.to_datetime(date_history[day]) then adding a value to it will fail and we need to assign a new value there (except block)
         try:
-            stock_data[pd.to_datetime(date_history[day])] += (arr_of_day_price[day] - arr_of_day_price[day - 1]) / \
+
+            stock_data[pd.to_datetime(date_history[day]).date()] += (arr_of_day_price[day] - arr_of_day_price[day - 1]) / \
                                                              arr_of_day_price[day - 1]
         except:
-            stock_data[pd.to_datetime(date_history[day])] = (arr_of_day_price[day] - arr_of_day_price[day - 1]) / \
+            stock_data[pd.to_datetime(date_history[day]).date()] = (arr_of_day_price[day] - arr_of_day_price[day - 1]) / \
                                                             arr_of_day_price[day - 1]
-    print(stock_data)
+    # print(stock_data)
     return stock_data
 
 
@@ -43,12 +45,14 @@ def stocks_returns(assets, weights, from_date, to_date):
     asset_number = 0
     arr_of_day_price = []
     for asset in assets:
-        history = yf.Ticker(asset).history(start=pd.to_datetime(from_date), end=pd.to_datetime(to_date) + pd.DateOffset(1))
+        history = yf.Ticker(asset).history(start=pd.to_datetime(from_date) - pd.DateOffset(1),
+                                           end=pd.to_datetime(to_date) + pd.DateOffset(1))
         date_history = history.index
         price_history = history["Close"]
 
         for price in range(0, len(price_history)):
             if (len(arr_of_day_price) - 1 < price):
+
                 arr_of_day_price.append(price_history[price] * weights[asset_number % len(assets)])
             else:
                 arr_of_day_price[price] += price_history[price] * weights[asset_number % len(assets)]
@@ -57,11 +61,16 @@ def stocks_returns(assets, weights, from_date, to_date):
     for day in range(1, len(price_history)):
         # if stock_data doesn't have an element at index date_history[day] then adding a value to it will fail and we need to assign a new value there
         try:
-            stock_data[date_history[day]] += (arr_of_day_price[day]-arr_of_day_price[day-1])/arr_of_day_price[day-1]
+            # print(f"1 = {date_history[day]}")
+            stock_data[date_history[day].date()] += (arr_of_day_price[day] - arr_of_day_price[day - 1]) / arr_of_day_price[
+                day - 1]
         except:
-            stock_data[date_history[day]] = (arr_of_day_price[day]-arr_of_day_price[day-1])/arr_of_day_price[day-1]
-    print(stock_data)
+            # print(f"2 = {date_history[day]}")
+            stock_data[date_history[day].date()] = (arr_of_day_price[day] - arr_of_day_price[day - 1]) / arr_of_day_price[
+                day - 1]
+    # print(stock_data)
     return stock_data
+
 
 # stocks_returns(["AAPL"], [1], "9/1/2022", "09/07/2022")
 
@@ -75,4 +84,5 @@ def cryptocurrencies_returns(assets, weights, from_date, to_date):
 
 
 # cryptocurrencies_returns(["ETH", "BTC"], [0.3, 0.7], "09/01/2022", "09/07/2022")
+
 
