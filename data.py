e696f6d07dd91752051456d33dbe20077b8093a3
walkrunman <walkrunman@gmail.com>
2022-10-17 21:56:51 +0300

    requirements and correction of a little mistake

--- data.py
+++ data.py
@@ -14,39 +14,37 @@ def download_and_count(assets: list, weights: list,
     from_date = tmp[1]+"/"+tmp[2]+"/"+tmp[0]
    
     def __procedure(asset):    
-        search_results = search_assets(query=assets, limit=20)
+        search_results = search_assets(query=asset, limit=2000)
         investing_id = int(search_results[0]["ticker"]) 
         data = historical_data(investing_id=investing_id, 
                                from_date=from_date, to_date=to_date)
         
-        df = pd.DataFrame(data)
-        df["date"] = pd.to_datetime(df["date"])
-        df = df[["date", "close"]]
-        df.columns = ["date", asset]
+        df_ = pd.DataFrame(data)
+        df_["date"] = pd.to_datetime(df_["date"])
+        df_ = df_[["date", "close"]]
+        df_.columns = ["date", asset]
         
-        return df
+        return df_
     
     df = __procedure(assets[0])
     assets = assets[1:]
     
+    
     if assets:
         for asset in assets:
-            tmp = __procedure(assets[0])
+            tmp = __procedure(asset)
             df = df.merge(tmp, on="date", how="left")
     
     columns = df.columns[1:]
     
-    # average weighted price
     for k,weight in enumerate(weights):
         df[columns[k]] = df[columns[k]]*weight
     
     df["price"] = df[columns].sum(axis=1)
     df["return"] = df["price"].diff()/df["price"].shift()
     
-    # df = df.fillna(-0.0136)
     df = df.dropna()
     
-    df["return"] = df["return"].map(lambda x: round(x, 4))
     df["return"] = df["return"].astype("float64")
     df = df[["date", "return"]].set_index("date")
     
