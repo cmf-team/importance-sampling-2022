d0204447e8f225468784c2019278f891ecbb9ac4
danyzolotarev@gmail.com <danyzolotarev@gmail.com>
2022-10-18 14:13:39 +0300

    different exchanges

--- data.py
+++ data.py
@@ -6,14 +6,14 @@ from datetime import datetime as dt
 import investiny as inv
 
 
-def calculate_returns(assets, weights, from_date, to_date):
+def calculate_returns(assets, weights, from_date, to_date, exchange=None):
     from_date = dt.strftime(dt.strptime(from_date, '%m/%d/%Y') - datetime.timedelta(days=1), '%m/%d/%Y')
     weights = np.array(weights)
     weights = weights / weights.sum()
     portfolio = pd.DataFrame(columns=['date', 'value'])
 
     for i, asset in enumerate(assets):
-        id = inv.search_assets(query=asset, limit=1, exchange="NASDAQ")[0]["ticker"]
+        id = inv.search_assets(query=asset, limit=1, exchange=exchange)[0]["ticker"]
         data = pd.DataFrame(inv.historical_data(investing_id=id, from_date=from_date, to_date=to_date))[
             ['date', 'close']]
 
@@ -28,11 +28,11 @@ def calculate_returns(assets, weights, from_date, to_date):
 
 
 def stocks_returns(assets, weights, from_date, to_date):
-    return calculate_returns(assets, weights, from_date, to_date)
+    return calculate_returns(assets, weights, from_date, to_date, exchange='NASDAQ')
 
 
 def commodities_returns(assets, weights, from_date, to_date):
-    return calculate_returns(assets, weights, from_date, to_date)
+    return calculate_returns(assets, weights, from_date, to_date, exchange='Commodity')
 
 
 def cryptocurrencies_returns(assets, weights, from_date, to_date):
