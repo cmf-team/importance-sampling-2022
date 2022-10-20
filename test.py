4580f6763510370c83dcc51fad0d3df6f560ccaf
mmssss <mmssss@disroot.org>
2022-10-20 16:25:00 +0300

    Small fix

--- test.py
+++ test.py
@@ -7,16 +7,16 @@ def test_stocks_returns():
     weights = [1.]
     returns = stocks_returns(assets, weights, from_date='09/02/2022', to_date='09/07/2022')
     test_returns = pd.Series(
-        data=[-0.0136, -0.0082, 0.0093],
+        data=[-0.0136, -0.0082, 0.0093], 
         index=pd.to_datetime(['09/02/2022', '09/06/2022', '09/07/2022']),
     )
     assert np.allclose(returns, test_returns, atol=0.0001)
-
+    
     assets = ['AAPL', 'GOOGL']
     weights = [0.3, 0.7]
     returns = stocks_returns(assets, weights, from_date='09/02/2022', to_date='09/07/2022')
     test_returns = pd.Series(
-        data=[-0.0158, -0.0091, 0.0188],
+        data=[-0.0158, -0.0091, 0.0188], 
         index=pd.to_datetime(['09/02/2022', '09/06/2022', '09/07/2022']),
     )
     assert np.allclose(returns, test_returns, atol=0.0001)
@@ -34,7 +34,7 @@ def test_commodities_returns():
 
     assets = [
         'Brent Oil', 'Crude Oil WTI', 'Natural Gas',
-        'Heating Oil', 'Gold', 'Silver', 'Copper',
+        'Heating Oil', 'Gold', 'Silver', 'Copper', 
         'Platinum', 'US Coffee C', 'US Corn'
     ]
     weights = np.ones(10)
