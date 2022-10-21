8deae248d1721aa805b4935fbf0da1b8c9f8161e
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2022-10-21 14:30:21 +0600

    Update tests

--- test.py
+++ test.py
@@ -35,13 +35,13 @@ def test_commodities_returns():
     assets = [
         'Brent Oil', 'Crude Oil WTI', 'Natural Gas',
         'Heating Oil', 'Gold', 'Silver', 'Copper', 
-        'Platinum', 'US Coffee C', 'US Corn'
+        'US Coffee C', 'US Corn'
     ]
-    weights = np.ones(10)
+    weights = np.ones(9)
     returns = commodities_returns(assets, weights, from_date='09/02/2022', to_date='09/07/2022')
     test_returns = pd.Series(
-        data=[ 0.0092, -0.2725,  0.2961,  0.0656,  0.0017],
-        index=pd.to_datetime(['2022-09-02', '2022-09-04', '2022-09-05', '2022-09-06', '2022-09-07']),
+        data=[ 0.0075,  0.001 , -0.0021],
+        index=pd.to_datetime(['2022-09-02', '2022-09-06', '2022-09-07']),
     )
     assert np.allclose(returns, test_returns, atol=0.0001)
 
