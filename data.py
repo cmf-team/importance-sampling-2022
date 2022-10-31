7d23667b67108bb1282f1a370f4850ae87adfdff
barracudada <skirill720@yandex.ru>
2022-10-31 22:07:40 +0300

    first task

--- data.py
+++ data.py
@@ -1,45 +1,53 @@
 import pandas as pd
 import numpy as np
-import investiny
+
+class Dataloader:
+    def __init__(
+            self,
+            series: pd.Series,
+            window_size: int,
+            step_size: int,
+            horizon: int,
+            first_pred: int
+    ):
+        self.series = series
+        self.window_size = window_size
+        self.step_size = step_size
+        self.horizon = horizon
+        self.first_pred = first_pred
+        assert self.first_pred > self.window_size
+        feat_idx = []
+        target_idx = []
+        for i in range(self.first_pred, self.series.shape[0], self.step_size):
+            feat_idx.append(range(i - self.horizon - self.window_size, i - self.horizon))
+            target_idx.append(i)
+        self.feat_idx = feat_idx
+        self.target_idx = target_idx
+
+    def __len__(self):
+        return len(self.feat_idx)
+
+    def __iter__(self):
+        self.iter = 0
+        return self
+
+    def __next__(self):
+        if self.iter < len(self.feat_idx):
+            feat = self.series.iloc[self.feat_idx[self.iter]]
+            target = self.series.iloc[self.target_idx[self.iter]]
+            self.iter += 1
+            return feat, target
+        else:
+            raise StopIteration
+
 
 def stocks_returns(assets, weights, from_date, to_date):
-    st_data = []
-    i = 0
-    for sec in assets:
-        sec_id = investiny.search_assets(query=sec, limit=1, exchange="NASDAQ")[0]['ticker']
-        data = investiny.historical_data(sec_id, from_date, to_date)
-        dates = data['date']
-        st_data.append([j * weights[i] for j in data['close']])
-        i += 1
-    res = pd.DataFrame(data = np.array(st_data), columns = dates).T 
-    res = res.sum(axis=1)
-    res = (res.diff()/res.shift(1)).dropna()
-    return(res)
+    raise Exception(NotImplementedError)
+
 
 def commodities_returns(assets, weights, from_date, to_date):
-    st_data = []
-    i = 0
-    for sec in assets:
-        sec_id = investiny.search_assets(query=comm[0], limit=1, type='Commodity')[0]["ticker"]
-        data = investiny.historical_data(sec_id, from_date, to_date)
-        dates = data['date']
-        st_data.append([j * weights[i] for j in data['close']])
-        i += 1
-    res = pd.DataFrame(data = np.array(st_data), columns = dates).T 
-    res = res.sum(axis=1)
-    res = (res.diff()/res.shift(1)).dropna()
-    return(res)
+    raise Exception(NotImplementedError)
+
 
 def cryptocurrencies_returns(assets, weights, from_date, to_date):
-    st_data = []
-    i = 0
-    for sec in assets:
-        sec_id = investiny.search_assets(query=comm[0], limit=1)[0]["ticker"]
-        data = investiny.historical_data(sec_id, from_date, to_date)
-        dates = data['date']
-        st_data.append([j * weights[i] for j in data['close']])
-        i += 1
-    res = pd.DataFrame(data = np.array(st_data), columns = dates).T 
-    res = res.sum(axis=1)
-    res = (res.diff()/res.shift(1)).dropna()
-    return(res)
+    raise Exception(NotImplementedError)
