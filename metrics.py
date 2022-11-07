9f6070f3956f26884e3935768c224f811c5421fc
Lisevi <77617453+Lisevi@users.noreply.github.com>
2022-11-07 20:06:08 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -4,7 +4,7 @@ import numpy as np
 def pof_test(var, target, alpha):
     '''Kupiec’s Proportion of Failures Test
        This statistic is asymptotically distributed as a chi-square variable with 1 degree of freedom'''
-    result = [(m < n) for (m,n) in zip(target, var)]
+    result = target < var
     
     total_observations = len(result)
     fail_observations = np.sum(result)
@@ -13,7 +13,7 @@ def pof_test(var, target, alpha):
     denominator = ((1 - fail_observations/total_observations)**(total_observations - fail_observations))*((fail_observations/total_observations)**fail_observations)
     stat = -2 * np.log(numerator/denominator)
     
-    return chi2.pdf(stat, df = 1)
+    return 1 - chi2.cdf(stat, df = 1)
 
 
 def if_test(var, target):
@@ -24,7 +24,7 @@ def if_test(var, target):
     n_10 = 0
     n_11 = 0
     
-    result = [(m < n) for (m,n) in zip(target, var)]
+    result = target < var
     result = list(reversed(result))
     for iter_ in range(len(result)-1):
         if result[iter_]:
@@ -38,14 +38,14 @@ def if_test(var, target):
             else:
                 n_00 += 1
     pi_0 = n_01/(n_01 + n_00)
-    pi_1 = n_10/(n_11 + n_10)
+    pi_1 = n_11/(n_11 + n_10)
     pi = np.sum(result)/len(result)
     
     numerator = ((1-pi)**(n_00 + n_10))*((pi)**(n_01+n_11))
     denominator = ((1 - pi_0)**n_00)*(pi_0**n_01)*((1-pi_1)**n_10)*(pi_1**n_11)
     stat = -2 * np.log(numerator/denominator)
     
-    return chi2.pdf(stat, df = 1)
+    return 1 - chi2.cdf(stat, df = 1)
 
 
 def quantile_loss(var, target, alpha):
