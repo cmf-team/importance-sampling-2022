0b9f953d4e0c805add766ef2f815050fe0914b75
Aleksandra Konstantinova CMF <115409738+ai-konstantinova@users.noreply.github.com>
2022-11-08 04:17:23 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import numpy as np
 from arch import arch_model
 from scipy.stats import norm
 
@@ -8,13 +9,18 @@ class RiskMetrics:
     document." Morgan Guaranty Trust Company of New York: New York 51
     (1996): 54.
     '''
+
     def __init__(self, alpha):
         self.alpha = alpha
         self.lambd = 0.94
         self.window_size = 74
 
     def forecast(self, feat):
-        raise Exception(NotImplementedError)
+        sigma_squared = np.zeros(len(feat))
+        for i in range(1, len(feat)):
+            sigma_squared = self.lambd * sigma_squared[i - 1] + (1 - self.lambd) * feat[i - 1] ** 2
+        sigma_sq = sigma_squared[-1]
+        return norm.ppf(1 - self.alpha, scale=sigma_sq ** 0.5)
 
 
 class HistoricalSimulation:
@@ -23,7 +29,7 @@ class HistoricalSimulation:
         self.window_size = window_size
 
     def forecast(self, feat):
-        raise Exception(NotImplementedError)
+        return np.quantile(feat[-self.window_size:], q=1 - self.alpha)
 
 
 class GARCH11:
@@ -35,4 +41,4 @@ class GARCH11:
         model = arch_model(feat[-self.window_size:], p=1, q=1, rescale=False)
         res = model.fit(disp='off')
         sigma2 = res.forecast(horizon=1, reindex=False).variance.values[0, 0]
-        return norm.ppf(1 - self.alpha, scale=sigma2**0.5)
+        return norm.ppf(1 - self.alpha, scale=sigma2 ** 0.5)
