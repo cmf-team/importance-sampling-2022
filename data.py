c681a36af400272723ea3e6eab47ec526da747e4
Denis Kornyukhov <63582828+hornyhov@users.noreply.github.com>
2022-11-12 14:38:03 +0300

    Update data.py

--- data.py
+++ data.py
@@ -6,6 +6,46 @@ from datetime import timedelta
 from pandas_datareader import data as pdr
 
 
+class Dataloader:
+    def __init__(
+            self,
+            series: pd.Series,
+            window_size: int,
+            step_size: int,
+            horizon: int,
+            first_pred: int
+    ):
+        self.series = series
+        self.window_size = window_size
+        self.step_size = step_size
+        self.horizon = horizon
+        self.first_pred = first_pred
+        assert self.first_pred > self.window_size
+        feat_idx = []
+        target_idx = []
+        for i in range(self.first_pred, self.series.shape[0], self.step_size):
+            feat_idx.append(range(i - self.horizon - self.window_size, i - self.horizon))
+            target_idx.append(i)
+        self.feat_idx = feat_idx
+        self.target_idx = target_idx
+
+    def __len__(self):
+        return len(self.feat_idx)
+
+    def __iter__(self):
+        self.iter = 0
+        return self
+
+    def __next__(self):
+        if self.iter < len(self.feat_idx):
+            feat = self.series.iloc[self.feat_idx[self.iter]]
+            target = self.series.iloc[self.target_idx[self.iter]]
+            self.iter += 1
+            return feat, target
+        else:
+            raise StopIteration
+            
+
 def get_portfolio(assets, weights, start, end):
     yf.pdr_override()
     data = pdr.get_data_yahoo(assets,
