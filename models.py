8ede3db2d00cb1af3e777632e4b500c65591eb9d
barracudada <skirill720@yandex.ru>
2022-11-14 01:49:28 +0300

    task 2

--- models.py
+++ models.py
@@ -1,20 +1,21 @@
 from arch import arch_model
 from scipy.stats import norm
+import numpy as np
 
 
 class RiskMetrics:
-    '''
-    Longerstaey, Jacques, and Martin Spencer. "Riskmetricstm—technical
-    document." Morgan Guaranty Trust Company of New York: New York 51
-    (1996): 54.
-    '''
     def __init__(self, alpha):
         self.alpha = alpha
         self.lambd = 0.94
         self.window_size = 74
 
     def forecast(self, feat):
-        raise Exception(NotImplementedError)
+        distribution = feat[-self.window_size:]
+        sigma2 = 0
+        for i in range(len(distribution)):
+            sigma2 = self.lambd * sigma2 + (1 - self.lambd) * distribution[i] ** 2
+        VaR = norm.ppf(1 - self.alpha, scale=np.sqrt(sigma2))
+        return VaR
 
 
 class HistoricalSimulation:
@@ -23,7 +24,9 @@ class HistoricalSimulation:
         self.window_size = window_size
 
     def forecast(self, feat):
-        raise Exception(NotImplementedError)
+        distribution = feat[-self.window_size:]
+        VaR = np.quantile(distribution, 1 - self.alpha)
+        return VaR
 
 
 class GARCH11:
@@ -35,4 +38,5 @@ class GARCH11:
         model = arch_model(feat[-self.window_size:], p=1, q=1, rescale=False)
         res = model.fit(disp='off')
         sigma2 = res.forecast(horizon=1, reindex=False).variance.values[0, 0]
-        return norm.ppf(1 - self.alpha, scale=sigma2**0.5)
+        VaR = norm.ppf(1 - self.alpha, scale=np.sqrt(sigma2))
+        return VaR
