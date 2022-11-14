57ee113881d6dbf0b2b7bc914294f1f0f8aaae39
v-argunov <argunovvlad5@gmail.com>
2022-11-14 19:15:30 +0300

    all tests passed corrected

--- test.py
+++ test.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 from data import (
-    stocks_returns, 
-    commodities_returns, 
-    cryptocurrencies_returns, 
+    stocks_returns,
+    commodities_returns,
+    cryptocurrencies_returns,
     Dataloader
 )
 from metrics import pof_test, if_test, quantile_loss
 from models import HistoricalSimulation, RiskMetrics
+
 
 class TestData:
     def test_stocks_returns(self):
@@ -15,16 +16,16 @@ class TestData:
         weights = [1.]
         returns = stocks_returns(assets, weights, from_date='09/02/2022', to_date='09/07/2022')
         test_returns = pd.Series(
-            data=[-0.0136, -0.0082, 0.0093], 
+            data=[-0.0136, -0.0082, 0.0093],
             index=pd.to_datetime(['09/02/2022', '09/06/2022', '09/07/2022']),
         )
         assert np.allclose(returns, test_returns, atol=0.0001)
-    
+
         assets = ['AAPL', 'GOOGL']
         weights = [0.3, 0.7]
         returns = stocks_returns(assets, weights, from_date='09/02/2022', to_date='09/07/2022')
         test_returns = pd.Series(
-            data=[-0.0158, -0.0091, 0.0188], 
+            data=[-0.0158, -0.0091, 0.0188],
             index=pd.to_datetime(['09/02/2022', '09/06/2022', '09/07/2022']),
         )
         assert np.allclose(returns, test_returns, atol=0.0001)
@@ -41,7 +42,7 @@ class TestData:
     def test_commodities_returns(self):
         assets = [
             'Brent Oil', 'Crude Oil WTI', 'Natural Gas',
-            'Heating Oil', 'Gold', 'Silver', 'Copper', 
+            'Heating Oil', 'Gold', 'Silver', 'Copper',
             'US Coffee C', 'US Corn'
         ]
         weights = np.ones(9)
@@ -110,7 +111,7 @@ class TestModels:
         target = np.array(target)
         assert np.isclose(quantile_loss(var, target, alpha), 0.0038, atol=0.0001)
         assert if_test(var, target) > 0.05
-    
+
     def test_riskmetrics(self):
         alpha = 0.95
         rm = RiskMetrics(alpha)
